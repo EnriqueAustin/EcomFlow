a2367be559d787fbaddf180b7a9941a959955a49
Enrique Austin <34680513+EnriqueAustin@users.noreply.github.com>
2023-02-01 06:04:49 +0200

    built productscard.js

--- ecomflow/components/ProductCard.js
+++ ecomflow/components/ProductCard.js
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import { AntDesign } from "@expo/vector-icons";
+import { useColorScheme } from 'nativewind';
 
-export default function ProductCard() {
-  return (
-    <View>
-      <Text>ProductCard</Text>
-    </View>
-  )
+export default function ProductCard({
+    image,
+    category,
+    title,
+    price,
+    description,
+}) {
+    const [count, setCount] = React.useState(1);
+    const { colorScheme } = useColorScheme();
+    return (
+        <View className='w-full bg-red dark:bg-gray-50/10' >
+            <Text>{title}</Text>
+            <Text>{description}</Text>
+            <Text>{price}</Text>
+        </View>
+    )
 }
