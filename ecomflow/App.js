f5260bb616987105c4e84967c34a85d09f8b9ba9
Enrique Austin <34680513+EnriqueAustin@users.noreply.github.com>
2023-02-01 05:54:25 +0200

    added data and screens

--- ecomflow/App.js
+++ ecomflow/App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { Switch, Text, View } from 'react-native';
 import { useColorScheme } from 'nativewind';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import ProductList from './components/ProductsLists';
 
 export default function App() {
   const {colorScheme, toggleColorScheme} = useColorScheme();
   return (
-    <View className="flex-1 items-center justify-center bg-gary-200 dark:bg-black">
-      <Switch value={colorScheme === "dark"} onChange={toggleColorScheme} />
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-gary-200 dark:bg-black">
+      <View className='flex-row w-full gap-5'>
+        <Text className='text-2xl font-bold dark:text-white' >
+          New collection
+        </Text>
+        <Switch value={colorScheme === "dark"} onChange={toggleColorScheme} />
+      </View>
+      <ProductList />
+      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+    </SafeAreaView>
   );
 }
