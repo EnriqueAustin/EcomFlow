f1550e496b7e2795722e35748eb8336c68b78f3a
Enrique Austin <34680513+EnriqueAustin@users.noreply.github.com>
2023-02-01 03:11:57 +0200

    installed native wind & mode switch button

--- ecomflow/App.js
+++ ecomflow/App.js
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Switch, Text, View } from 'react-native';
+import { useColorScheme } from 'nativewind';
 
 export default function App() {
+  const {colorScheme, toggleColorScheme} = useColorScheme();
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-gary-200 dark:bg-black">
+      <Switch value={colorScheme === "dark"} onChange={toggleColorScheme} />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
